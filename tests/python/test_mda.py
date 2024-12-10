fc4a9c4a28220b97f0f67b8d9b151b6e9b3e1fbf
ssande7 <1731652+ssande7@users.noreply.github.com>
2024-12-10 16:14:28 +1000

    feat(tests): Add cmake harness for pytest

---
+++ tests/python/test_mda.py
@@ -0,0 +1,15 @@
+import pytest
+import dofulator as dof
+
+"""
+Testing of MDA interface
+"""
+
+class TestMDADofulator:
+    def test_trivial(self):
+        return True;
+
+
+class TestLocalTemperature:
+    def test_trivial(self):
+        return True;
